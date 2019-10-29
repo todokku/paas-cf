c56ce17e407d99c62c93d415a2bb51e07c710565
Mohamed Deerow <mohamed.deerow@digital.cabinet-office.gov.uk>
2019-10-29 13:05:06 +0000

    wip rds

--- terraform/cloudfoundry/cf_rds.tf
+++ terraform/cloudfoundry/cf_rds.tf
@@ -39,7 +39,7 @@ resource "aws_db_instance" "cf" {
   identifier           = "${var.env}-cf"
   allocated_storage    = 100
   engine               = "postgres"
-  engine_version       = "11.1"
+  engine_version       = "11.5"
   instance_class       = "${var.cf_db_instance_type}"
   username             = "dbadmin"
   password             = "${var.secrets_cf_db_master_password}"
@@ -55,9 +55,9 @@ resource "aws_db_instance" "cf" {
   skip_final_snapshot       = "${var.cf_db_skip_final_snapshot}"
   vpc_security_group_ids    = ["${aws_security_group.cf_rds.id}"]
 
-  allow_major_version_upgrade = false
-  auto_minor_version_upgrade  = false
-  apply_immediately           = false
+  allow_major_version_upgrade = true
+  auto_minor_version_upgrade  = true
+  apply_immediately           = true
 
   tags {
     Name       = "${var.env}-cf"
