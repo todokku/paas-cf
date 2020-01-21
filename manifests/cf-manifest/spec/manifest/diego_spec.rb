774cad7b141692bd742d17ada41373a7f49e41d2
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-01-21 17:01:40 +0000

    Resolve ambiguity in silk-release bosh links
    When we duplicate the diego-cells, we end up with multiple `vpa` link
    providers (i.e. vxlan-policy-agent provides `vpa` which is configured
    multiple times) and multiple `vpa` link consumers, (i.e. silk-cni and
    silk-daemon also want to consume `vpa`)
    
    We have to override the consumes/produces in the isolation segment
    instance groups
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
    Co-authored-by: Sebastian <sebastian.schmieschek@digital.cabinet-office.gov.uk>

--- manifests/cf-manifest/spec/manifest/diego_spec.rb
+++ manifests/cf-manifest/spec/manifest/diego_spec.rb
@@ -8,7 +8,63 @@ RSpec.describe "diego" do
     end
 
     it "has containers/proxy enabled" do
-      expect(properties.dig('containers', 'proxy', 'enabled')).to be(true)
+      expect(rep_properties.dig('containers', 'proxy', 'enabled')).to be(true)
+    end
+  end
+
+  context "ipsec" do
+    let(:racoon_props) { manifest_with_defaults.fetch("instance_groups.diego-cell.jobs.racoon.properties.racoon") }
+
+    it "has ipsec enabled with router instances" do
+      cell_network = racoon_props.dig('ports').find { |n| n['name'] == 'cell' }
+      router_cidrs = terraform_fixture_value('router_subnet_cidr_blocks')
+
+      router_cidrs.each do |cidr|
+        expect(cell_network['targets']).to include(cidr), "cell network should include router cidr #{cidr}"
+      end
+    end
+
+    it "has ipsec enabled with cell instances" do
+      cell_network = racoon_props.dig('ports').find { |n| n['name'] == 'cell' }
+      cell_cidrs   = terraform_fixture_value('cell_subnet_cidr_blocks')
+
+      cell_cidrs.each do |cidr|
+        expect(cell_network['targets']).to include(cidr), "cell network should include cell cidr #{cidr}"
+      end
+    end
+  end
+
+  context "silk-cni" do
+    let(:silk_cni) { manifest_with_defaults.fetch("instance_groups.diego-cell.jobs.silk-cni") }
+
+    it "overrides the vpa bosh link" do
+      expect(silk_cni.dig('consumes', 'vpa')).to eq('from' => 'vpa-default')
+    end
+
+    let(:silk_cni_props) { manifest_with_defaults.fetch("instance_groups.diego-cell.jobs.silk-cni.properties") }
+
+    it "accounts for the IPsec VPN when setting the MTU" do
+      vm_mtu = 9001
+      ipsec_overhead = 73
+
+      mtu = silk_cni_props['mtu']
+      expect(mtu).to be <= (vm_mtu - ipsec_overhead)
+    end
+  end
+
+  context "silk-daemon" do
+    let(:silk_daemon) { manifest_with_defaults.fetch("instance_groups.diego-cell.jobs.silk-daemon") }
+
+    it "overrides the vpa bosh link" do
+      expect(silk_daemon.dig('consumes', 'vpa')).to eq('from' => 'vpa-default')
+    end
+  end
+
+  context "vxlan-policy-agent" do
+    let(:vxlan_policy_agent) { manifest_with_defaults.fetch("instance_groups.diego-cell.jobs.vxlan-policy-agent") }
+
+    it "overrides the vpa bosh link" do
+      expect(vxlan_policy_agent.dig('provides', 'vpa')).to eq('as' => 'vpa-default')
     end
   end
 end
